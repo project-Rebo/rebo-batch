d0e3b2c088b0cae872e21b9871ea942b5acda7ad
kittae <jinkitea@naver.com>
2025-05-13 02:29:55 +0900

    chore: 이슈,pr 템플릿 설정 추가

---
+++ src/main/java/hello/rebobatch/ReboBatchApplication.java
@@ -0,0 +1,13 @@
+package hello.rebobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReboBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReboBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/rebobatch/ReboBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.rebobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReboBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
