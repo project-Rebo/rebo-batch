9e607cc8205c0d88c3c315f5bdd325d9373db72a
kittae <jinkitea@naver.com>
2025-05-21 13:34:39 +0900

    refactor: Store 엔티티 및 Repository 수정

--- src/main/java/hello/rebobatch/domain/store/entity/Store.java
+++ src/main/java/hello/rebobatch/domain/store/entity/Store.java
@@ -1,4 +1,75 @@
 package hello.rebobatch.domain.store.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "store")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 225)
+    private String csvId;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 50)
+    private String categoryLarge;
+    @Column(length = 50)
+    private String categoryMiddle;
+    @Column(length = 50)
+    private String categorySmall;
+
+    @Column(length = 255)
+    private String roadAddress;
+
+    @Column(name = "latitude", nullable = false, precision = 9, scale = 6)
+    private BigDecimal latitude;
+
+    @Column(name = "longitude", nullable = false, precision = 9, scale = 6)
+    private BigDecimal longitude;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
+
+    public Store(Long id,String csvId, String name, String categoryLarge, String categoryMiddle,
+                 String categorySmall, String roadAddress, BigDecimal latitude,
+                 BigDecimal longitude, LocalDateTime createdAt, LocalDateTime updatedAt,
+                 Boolean isDeleted) {
+        this.id = id;
+        this.csvId = csvId;
+        this.name = name;
+        this.categoryLarge = categoryLarge;
+        this.categoryMiddle = categoryMiddle;
+        this.categorySmall = categorySmall; // 오타 수정
+        this.roadAddress = roadAddress;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.isDeleted = (isDeleted != null) ? isDeleted : false;
+    }
 }
